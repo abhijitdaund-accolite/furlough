03023d9a585a01ecdd7b55898bb21e0e5a1a1a26
viggieAccolite <vignesh.b@accoliteindia.com>
2018-07-11 15:40:22 +0530

    Added a funciton to retrieve list of accolite Employees from list of MSIDs

--- furlough_backend/src/main/java/com/accolite/furlough/repository/AccoliteEmployeeRepository.java
+++ furlough_backend/src/main/java/com/accolite/furlough/repository/AccoliteEmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.accolite.furlough.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ public interface AccoliteEmployeeRepository
         extends
         JpaRepository<AccoliteEmployee, String> {
     AccoliteEmployee findByMSID(String mSId);
+
+    List<AccoliteEmployee> findByMSIDIn(List<String> mSID);
 }
