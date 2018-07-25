ea3876e5493915d2a4262e64dcf3c839e6302daf
viggieAccolite <vignesh.b@accoliteindia.com>
2018-07-25 14:41:39 +0530

    Backend ready!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Login left

--- furlough_backend/src/main/java/com/accolite/furlough/rest/ResponseController.java
+++ furlough_backend/src/main/java/com/accolite/furlough/rest/ResponseController.java
@@ -5,20 +5,24 @@ import java.util.Map;
 
 import javax.ws.rs.QueryParam;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.accolite.furlough.service.ResourceConfirmedService;
 import com.accolite.furlough.utils.EmailUtil;
 
 @RestController
 public class ResponseController {
+    @Autowired
+    private ResourceConfirmedService ResourceConfirmer;
 
     // http:10.4.12.142:8080/furlough/verify?msid=GHDF23&dates=jlalhlseef20349023
     @RequestMapping("/verify")
     @ResponseBody
-    public String update(@QueryParam(value = "msid") final String msid,
-            @QueryParam(value = "token") final String token) {
+    public String update(@QueryParam(value = "msid") final String msid, @QueryParam(value = "token") final String token)
+            throws Exception {
         if (msid == null || token == null) // Error Handling to be done
             return "The link has expired. Please contact the product owner for help";
 
@@ -26,7 +30,8 @@ public class ResponseController {
         final String decodedMSID = EmailUtil.base64Decode(msid);
         final Map<String, List<String>> map = EmailUtil.getMailResponseMap(decodedMSID, decodedDates);
         // Function which will use the above map!
-        return map.toString();
+        ResourceConfirmer.confirmer(map);
+        return "successFully confirmed in the database";
     }
 
 }
